6b8a27a764edf66ac078403d92a7b96f252526a2
TerrinTian-Spotec <terrin.tian@spotec.net>
2026-08-07 16:44:23 +0800

    feat: AI未来已来

--- src/routes/menu.tsx
+++ src/routes/menu.tsx
@@ -1,32 +1,27 @@
-import type { MenuProps } from 'antd';
-import companyPath from '@/routes/paths/company';
-import markdownPath from '@/routes/paths/markdown';
-import aiPath from '@/routes/paths/ai';
+import type { MenuProps } from "antd";
+import aiPath from "@/routes/paths/ai";
+import companyPath from "@/routes/paths/company";
+import markdownPath from "@/routes/paths/markdown";
 
-type MenuItem = Required<MenuProps>['items'][number];
+type MenuItem = Required<MenuProps>["items"][number];
 
 export const menuItems: MenuItem[] = [
-  {
-    key: 'company',
-    label: 'Company',
-    children: [
-      { key: companyPath.BASE, label: '常用' },
-    ],
-  },
-  {
-    key: 'markdown',
-    label: 'Markdown',
-    children: [
-      { key: markdownPath.DEMO, label: 'MDX 演示' },
-    ],
-  },
-  {
-    key: 'ai',
-    label: 'AI',
-    children: [
-      { key: aiPath.BASE, label: 'ClaudeCode' },
-      { key: aiPath.STUDY, label: '学习' },
-      
-    ],
-  },
+	{
+		key: "company",
+		label: "Company",
+		children: [{ key: companyPath.BASE, label: "常用" }],
+	},
+	{
+		key: "markdown",
+		label: "Markdown",
+		children: [{ key: markdownPath.DEMO, label: "MDX 演示" }],
+	},
+	{
+		key: "ai",
+		label: "AI",
+		children: [
+			{ key: aiPath.BASE, label: "ClaudeCode" },
+			{ key: aiPath.STUDY, label: "AI编程落地指南" },
+		],
+	},
 ];
